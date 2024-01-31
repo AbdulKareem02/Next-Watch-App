4b3789b330d4647388abb176639fa302b5ed82e6
AbdulKareem02 <shaik161412@gmail.com>
2024-01-31 07:16:09 +0000

    my app

---
+++ src/components/VideoCard/index.js
@@ -0,0 +1,65 @@
+import ThemeAndVideoContext from '../../context/ThemeAndVideoContext'
+
+import {
+  ItemLink,
+  TrendingListItem,
+  TrendingThumbNailImage,
+  TrendingVideoDetails,
+  TrendingProfileImage,
+  TrendingContentSection,
+  TrendingTitle,
+  TrendingChannelName,
+  TrendingViewsAndDate,
+  TrendingDOt,
+} from './styledComponents'
+
+const VideoCard = props => {
+  const {videoDetails} = props
+  const {
+    id,
+    title,
+    thumbnailUlr,
+    viewCount,
+    publishedAt,
+    name,
+    profileImageUrl,
+  } = videoDetails
+
+  return (
+    <ThemeAndVideoContext.Consumer>
+      {value => {
+        const {isDarkTheme} = value
+        const textColor = isDarkTheme ? '#f9f9f9' : '#231f20'
+
+        return (
+          <ItemLink to={`/videos/${id}`} className="link">
+            <TrendingListItem>
+              <TrendingThumbNailImage
+                src={thumbnailUlr}
+                alt="video thumbnail"
+              />
+              <TrendingVideoDetails>
+                <TrendingProfileImage
+                  src={profileImageUrl}
+                  alt="channel logo"
+                />
+                <TrendingContentSection>
+                  <TrendingTitle color={textColor}>{title}</TrendingTitle>
+                  <TrendingChannelName color={textColor}>
+                    {name}
+                  </TrendingChannelName>
+                  <TrendingViewsAndDate color={textColor}>
+                    {viewCount} views<TrendingDOt>&#8226;</TrendingDOt>
+                    {publishedAt}
+                  </TrendingViewsAndDate>
+                </TrendingContentSection>
+              </TrendingVideoDetails>
+            </TrendingListItem>
+          </ItemLink>
+        )
+      }}
+    </ThemeAndVideoContext.Consumer>
+  )
+}
+
+export default VideoCard
